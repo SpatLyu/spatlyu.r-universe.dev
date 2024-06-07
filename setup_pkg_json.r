03b98a24f93ae6a54f49c2494192fd7a7f4f8057
SpatLyu <lyu.geosocial@gmail.com>
2024-06-07 12:52:19 +0800

    add gdverse package

--- setup_pkg_json.r
+++ setup_pkg_json.r
@@ -1,9 +1,10 @@
-setwd('c:/Users/dell/Downloads/pkgdev/r-uni/')
+# setwd('c:/Users/dell/Downloads/pkgdev/r-uni/')
 
 df = data.frame(
-  package = c("spEcula", "tidyrgeoda"),
+  package = c("spEcula", "tidyrgeoda", "gdverse"),
   url = c("https://github.com/SpatLyu/spEcula", 
-          "https://github.com/SpatLyu/tidyrgeoda"),
-  subdir = c(NA, NA)
+          "https://github.com/SpatLyu/tidyrgeoda",
+          "https://github.com/SpatLyu/gdverse"),
+  subdir = rep(NA,3)
 )
 jsonlite::write_json(df, 'packages.json', pretty = TRUE)
