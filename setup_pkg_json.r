52e64875bc09a4355cc2dc07d14868aeecda3d24
SpatLyu <lyu.geosocial@gmail.com>
2024-07-09 14:05:40 +0800

    add geocomplexity to r-universe

--- setup_pkg_json.r
+++ setup_pkg_json.r
@@ -1,10 +1,11 @@
 # setwd('c:/Users/dell/Downloads/pkgdev/r-uni/')
 
 df = data.frame(
-  package = c("spEcula", "tidyrgeoda", "gdverse"),
+  package = c("spEcula", "tidyrgeoda", "gdverse", "geocomplexity"),
   url = c("https://github.com/SpatLyu/spEcula", 
           "https://github.com/SpatLyu/tidyrgeoda",
-          "https://github.com/SpatLyu/gdverse"),
-  subdir = rep(NA,3)
+          "https://github.com/SpatLyu/gdverse",
+          "https://github.com/SpatLyu/geocomplexity"),
+  subdir = rep(NA,4)
 )
 jsonlite::write_json(df, 'packages.json', pretty = TRUE)
