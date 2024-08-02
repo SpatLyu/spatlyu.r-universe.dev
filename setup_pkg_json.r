2958af5e1824f577b61bf4b85116e8e7f2430efa
SpatLyu <lyu.geosocial@gmail.com>
2024-08-03 02:02:58 +0800

    add geocn package

--- setup_pkg_json.r
+++ setup_pkg_json.r
@@ -1,11 +1,12 @@
 # setwd('c:/Users/dell/Downloads/pkgdev/r-uni/')
 
 df = data.frame(
-  package = c("spEcula", "tidyrgeoda", "gdverse", "geocomplexity"),
+  package = c("spEcula", "tidyrgeoda", "gdverse", "geocomplexity", "geocn"),
   url = c("https://github.com/SpatLyu/spEcula", 
           "https://github.com/SpatLyu/tidyrgeoda",
           "https://github.com/SpatLyu/gdverse",
-          "https://github.com/SpatLyu/geocomplexity"),
-  subdir = rep(NA,4)
+          "https://github.com/SpatLyu/geocomplexity",
+          "https://github.com/SpatLyu/geocn"),
+  subdir = rep(NA,5)
 )
 jsonlite::write_json(df, 'packages.json', pretty = TRUE)
