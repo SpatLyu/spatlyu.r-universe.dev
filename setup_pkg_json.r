3c349881aa7f751bee223c8ec438ecf0ddd8cf90
SpatLyu <lyu.geosocial@gmail.com>
2024-08-08 16:59:18 +0800

    add sdsfun package

--- setup_pkg_json.r
+++ setup_pkg_json.r
@@ -2,12 +2,14 @@
 # setwd('d:/download/pkgdev/r-uni/')
 
 df = data.frame(
-  package = c("spEcula", "tidyrgeoda", "gdverse", "geocomplexity", "geocn"),
+  package = c("spEcula", "tidyrgeoda", "gdverse", 
+              "geocomplexity", "geocn", "sdsfun"),
   url = c("https://github.com/SpatLyu/spEcula", 
           "https://github.com/SpatLyu/tidyrgeoda",
           "https://github.com/SpatLyu/gdverse",
           "https://github.com/SpatLyu/geocomplexity",
-          "https://github.com/SpatLyu/geocn"),
-  subdir = rep(NA,5)
+          "https://github.com/SpatLyu/geocn",
+          "https://github.com/SpatLyu/sdsfun"),
+  subdir = rep(NA,6)
 )
 jsonlite::write_json(df, 'packages.json', pretty = TRUE)
