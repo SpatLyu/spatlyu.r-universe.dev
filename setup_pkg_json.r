9d8d1274c3ff5fc78f9f72e9e281509db1f9b43e
SpatLyu <lyu.geosocial@gmail.com>
2024-08-20 00:45:01 +0800

    move gdverse and geocomplexity to ausgis

--- setup_pkg_json.r
+++ setup_pkg_json.r
@@ -2,14 +2,11 @@
 # setwd('d:/download/pkgdev/r-uni/')
 
 df = data.frame(
-  package = c("spEcula", "tidyrgeoda", "gdverse", 
-              "geocomplexity", "geocn", "sdsfun"),
+  package = c("spEcula", "tidyrgeoda","geocn", "sdsfun"),
   url = c("https://github.com/SpatLyu/spEcula", 
           "https://github.com/SpatLyu/tidyrgeoda",
-          "https://github.com/SpatLyu/gdverse",
-          "https://github.com/SpatLyu/geocomplexity",
           "https://github.com/SpatLyu/geocn",
           "https://github.com/SpatLyu/sdsfun"),
-  subdir = rep(NA,6)
+  subdir = rep(NA,4)
 )
 jsonlite::write_json(df, 'packages.json', pretty = TRUE)
