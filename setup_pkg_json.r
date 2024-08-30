3e47bcec7e4085878a463e4fac3dba4e4fb58c9a
SpatLyu <lyu.geosocial@gmail.com>
2024-08-31 00:44:32 +0800

    migrate geocn to stscl

--- setup_pkg_json.r
+++ setup_pkg_json.r
@@ -2,11 +2,10 @@
 # setwd('d:/download/pkgdev/r-uni/')
 
 df = data.frame(
-  package = c("spEcula", "tidyrgeoda","geocn", "sdsfun"),
+  package = c("spEcula", "tidyrgeoda", "sdsfun"),
   url = c("https://github.com/SpatLyu/spEcula", 
           "https://github.com/SpatLyu/tidyrgeoda",
-          "https://github.com/SpatLyu/geocn",
           "https://github.com/SpatLyu/sdsfun"),
-  subdir = rep(NA,4)
+  subdir = rep(NA,3)
 )
 jsonlite::write_json(df, 'packages.json', pretty = TRUE)
