2b142e1a811fcc7163902a42d9c62f8f3e02920c
SpatLyu <lyu.geosocial@gmail.com>
2024-12-04 23:02:56 +0800

    add `HSAR` pkg

--- setup_pkg_json.r
+++ setup_pkg_json.r
@@ -2,9 +2,10 @@
 # setwd('d:/download/pkgdev/r-uni/')
 
 df = data.frame(
-  package = c("spEcula", "tidyrgeoda"),
+  package = c("spEcula", "tidyrgeoda", "HSAR"),
   url = c("https://github.com/SpatLyu/spEcula", 
-          "https://github.com/SpatLyu/tidyrgeoda"),
-  subdir = rep(NA,2)
+          "https://github.com/SpatLyu/tidyrgeoda",
+          "https://github.com/SpatLyu/HSAR"),
+  subdir = rep(NA,3)
 )
 jsonlite::write_json(df, 'packages.json', pretty = TRUE)
